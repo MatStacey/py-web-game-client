3306cdd4b372c02891a2f9d8962cf3983da43e4b
Mathew Stacey <mathew.j.stacey@gmail.com>
2022-07-10 19:16:31 +0100

    refactor - moved clientproperties into own file. Moved functions into class

---
+++ clientproperties.py
@@ -0,0 +1,34 @@
+
+
+from PyQt5.QtGui import QIcon
+import json
+import os
+
+class ClientProperties():
+    def __init__(self):
+        self.url = "https://universe.flyff.com/play"
+        self.app_name = "pyFlyffClient"
+        self.icon_file = "icons/PyFlyff.ico"
+        self.min_window_size = { "width" : 640, "height" : 480 }
+
+    def get_name(self):
+        return self.app_name
+
+    def get_url(self):
+        return self.url
+
+    def get_icon(self):
+        return self.icon_file
+
+    def load_vk_code(self):
+        config_directory = os.path.dirname(__file__) + '/config'
+        vk_code_json_path = os.path.join(config_directory, 'vk_code.json')
+        with open(vk_code_json_path, 'r') as json_file:
+            vk_code_map = json.load(json_file)
+        return vk_code_map
+    
+    def set_window_properties(self, window):
+        window.browser = None
+        window.setWindowIcon(QIcon(self.get_icon()))
+        window.setMinimumSize(self.min_window_size['width'], self.min_window_size['height'])
+        return window
--- main.py
+++ main.py
@@ -1,57 +1,37 @@
-import json,sys, os
+import sys
 
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtGui import QIcon
-
-class ApplicationProperties():
-    def __init__(self):
-        self.url = "https://universe.flyff.com/play"
-        self.app_name = "pyFlyffClient"
-        self.icon_file = "icons/PyFlyff.ico"
-
-    def get_name(self):
-        return self.app_name
-
-    def get_url(self):
-        return self.url
-
-    def get_icon(self):
-        return self.icon_file
-
-    def load_vk_code(self):
-        config_directory = os.path.dirname(__file__) + '/config'
-        vk_code_json_path = os.path.join(config_directory, 'vk_code.json')
-        with open(vk_code_json_path, 'r') as json_file:
-            vk_code_map = json.load(json_file)
-        return vk_code_map
+import clientproperties
 
 class MainWindow(QMainWindow):
 
-    min_window_size = { "width" : 640, "height" : 480 }
     default_user_agent = "None"
 
-    def __init__(self, properties):
+    def __init__(self, props):
         super(MainWindow, self).__init__()
-        self.set_main_window_properties(properties)
-        self.vk_code_map = properties.load_vk_code()
+
+        self.properties = props
+        self.vk_code_map = props.load_vk_code()
+
+        self.set_main_window_properties(self.properties)
+
         self.load_user_agent()
         self.load_menu()
 
-        self.load_flyff_homepage()
+        self.load_flyff_homepage(self.properties)
         return
 
-    def set_main_window_properties(self, properties):
-        self.browser = None
-        self.setWindowIcon(QIcon(properties.get_icon()))
-        self.setMinimumSize(MainWindow.min_window_size['width'], MainWindow.min_window_size['height'])
-        self.showMaximized()
+    def set_main_window_properties(self, props):
+        window = props.set_window_properties(self)
+        window.showMaximized()
+
     
     def load_user_agent(self):
         return MainWindow.default_user_agent
 
-    def load_flyff_homepage(self):
+    def load_flyff_homepage(self, properties):
         if not self.browser or self.browser.strip() == "":
             self.browser = None
 
@@ -66,7 +46,7 @@ class MainWindow(QMainWindow):
 
 
 app = QApplication(sys.argv)
-properties = ApplicationProperties()
-QApplication.setApplicationName(properties.get_name())
-window = MainWindow(properties)
+app_properties = clientproperties.ClientProperties()
+QApplication.setApplicationName(app_properties.get_name())
+window = MainWindow(app_properties)
 app.exec_()
